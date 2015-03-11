8f3eabfc99c6ba1fc3fd9768af2b2d4f47364ddd
Meghan Sweet <meghan.sweet@gmail.com>
2015-03-10 22:26:58 -0700

    Initial commit

---
+++ _site/js/script.js
@@ -0,0 +1,41 @@
+
+/* Mobile navigation
+   ========================================================================== */
+
+$( document ).ready(function() {
+
+	/*Mobile nav */
+    $( ".menu-icon" ).click(function() {
+  		$( ".main-nav ul" ).slideToggle( "fast", function() {
+  		});
+	});
+
+
+    var responsive_viewport = $(window).width();
+
+
+
+    /*Smilies in table */
+	if ($(window).width() > 600){	
+    
+    // Get number
+    $( "table.family-size td:nth-child(1)").slice(1).each(function( index ) {
+    	var text = $(this).text();
+		var number = parseInt(text, 10);
+
+	// Hide number
+		 $("p", this).hide();
+
+    // Build array
+        var smile = Array(number+1).join("<i class='fa fa-smile-o'></i>");
+
+    // Append to the parent.
+        $(this).append( smile );
+
+	});
+
+	}	
+
+
+
+});
